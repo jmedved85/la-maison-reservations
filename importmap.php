af9a773224801f4c4c75cf511e762fd6a145bbf0
Jasmin Medved <jmedved85@gmail.com>
2026-03-25 19:41:56 +0100

    composer update

--- importmap.php
+++ importmap.php
@@ -20,13 +20,13 @@ return [
         'path' => './assets/app_admin.js',
         'entrypoint' => true,
     ],
-    // '@hotwired/stimulus' => [
-    //     'version' => '3.2.2',
-    // ],
-    // '@symfony/stimulus-bundle' => [
-    //     'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    // ],
-    // '@hotwired/turbo' => [
-    //     'version' => '7.3.0',
-    // ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
 ];
